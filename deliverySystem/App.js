384ffd041bed2a30f10ff65d45dd2f70c9cde449
Fábio Lira <fabiolira1910@hotmail.com>
2019-11-06 22:45:57 -0300

    changing app name
    some hours spended changing the name and adding some icons.
    yes, first time doing this.

--- deliverySystem/App.js
+++ deliverySystem/App.js
@@ -1,15 +1,36 @@
+import React from 'react';
 import {
     createAppContainer,
 } from 'react-navigation';
 
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
 import CadastroItemScreen from './Screens/CadastroItemScreen/CadastroItemScreen'
 import ListagemItemScreen from './Screens/ListagemItemScreen/ListagemItemScreen'
 
-const TabNavigator = createBottomTabNavigator({
-    Cadastro: CadastroItemScreen,
-    Listagem: ListagemItemScreen,
-});
+const TabNavigator = createBottomTabNavigator(
+    {
+        Cadastro: CadastroItemScreen,
+        Listagem: ListagemItemScreen,
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: () => {
+                const { routeName } = navigation.state;
+                let IconComponent = Ionicons;
+                let iconName;
+                if (routeName === 'Cadastro') {
+                    iconName = 'ios-add';
+                } else if (routeName === 'Listagem') {
+                    iconName = 'ios-list';
+                }
+
+                return <IconComponent name={iconName} size={25} color={'grey'} />;
+            },
+        })
+    }
+);
 
 export default createAppContainer(TabNavigator);
