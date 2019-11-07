cc3f1cb7ac07328efd9da45ce3bbbc99331ab929
Fábio Lira <fabiolira1910@hotmail.com>
2019-11-07 18:57:31 -0300

    base design commit
    as the name says.

--- deliverySystem/Screens/ListagemItemScreen/ListagemItemScreen.js
+++ deliverySystem/Screens/ListagemItemScreen/ListagemItemScreen.js
@@ -1,16 +1,74 @@
 import React from 'react';
 import {
+    ScrollView,
     View,
-    Text,
+    ImageBackground,
+    StyleSheet,
+    Text
 } from 'react-native';
+import {
+    Container,
+    Card,
+    CardItem,
+    Body,
+} from 'native-base';
 
 
 export default class SettingsScreen extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Aqui ficará a listagem dos itens</Text>
-            </View>
+            <Container style={{ flex: 1 }}>
+                <ImageBackground
+                    source={require('../../assets/background.jpg')}
+                    resizeMode="cover"
+                    style={styles.image}
+                >
+                    <View style={styles.rect}>
+                        <View style={{ marginTop: 10, marginLeft: 10, marginBottom: 20 }}> 
+                            <Text style={{ fontSize: 17, fontWeight: '700' }}> 
+                                Listagem de Entregas 
+                            </Text>
+                        </View>
+                        <ScrollView style={{ marginLeft: 10, marginRight: 10 }}>
+                            <Card>
+                                <CardItem header bordered>
+                                    <Text>Nome do Cliente</Text>
+                                </CardItem>
+                                <CardItem bordered>
+                                    <Body>
+                                        <Text>
+                                            Origem: alguma coisa
+                                        </Text>
+                                        <Text style={{ marginTop: 10 }}>
+                                            Destino: alguma outra coisa
+                                        </Text>
+                                    </Body>
+                                </CardItem>
+                                <CardItem footer bordered>
+                                    <Text>Data limite qualquer</Text>
+                                </CardItem>
+                            </Card>
+                        </ScrollView>
+                    </View>
+                </ImageBackground>
+            </Container>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    image: {
+        width: 405,
+        height: 700,
+        alignSelf: "center"
+    },
+    rect: {
+        width: 350,
+        height: 584,
+        backgroundColor: "rgba(255,255,255,1)",
+        opacity: 0.80,
+        marginTop: 40,
+        alignSelf: 'center',
+    }
+});
