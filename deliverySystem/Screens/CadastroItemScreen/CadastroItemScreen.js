f8c69a19ea6d00c5162d78d71099efdc18543e23
Fábio Lira <fabiolira1910@hotmail.com>
2019-11-07 23:47:07 -0300

    working form and working code
    now the delivery register works with the local state component.
    added some logic to calculate the keys and clear the input boxes after pressing the 'add' button.
    some code cleaning.

--- deliverySystem/Screens/CadastroItemScreen/CadastroItemScreen.js
+++ deliverySystem/Screens/CadastroItemScreen/CadastroItemScreen.js
@@ -3,17 +3,19 @@ import {
 	StyleSheet,
 	View,
 	ImageBackground,
-	Text
+	Text,
+	ScrollView,
+	ToastAndroid
 } from "react-native";
-import { 
-	Container, 
-	Item, 
-	Input, 
-	DatePicker, Form, 
-	Content, 
+import {
+	Container,
+	Item,
+	Input,
+	Form,
+	Content,
 	Button,
 	Icon
- } from "native-base";
+} from "native-base";
 import TextInputMask from 'react-native-text-input-mask';
 
 export default class CadastroItemScreen extends Component {
@@ -21,8 +23,42 @@ export default class CadastroItemScreen extends Component {
 		someData: [],
 		newNome: null,
 		newData: null,
-		newPartida: null,
-		newDestino: null
+		newOrigem: null,
+		newDestino: null,
+		lastKey: 0
+	}
+
+	getLastKey = (dataArray = this.state.someData) => {
+		let count = 0;
+		dataArray.forEach(() => {
+			count++;
+		})
+		return count;
+	}
+
+	addNewItemInList = (state = this.state) => {
+		let newArray = state.someData;
+		newArray.push({
+			key: state.lastKey,
+			nome: state.newNome,
+			data: state.newData,
+			origem: state.newOrigem,
+			destino: state.newDestino
+		});
+		this.setState({ someData: newArray });
+		this.setState({ lastKey: this.getLastKey() });
+
+	}
+
+	clearAllInputBoxes = () => {
+		this.setState({ newData: "" });
+		this.setState({ newDestino: "" });
+		this.setState({ newNome: "" });
+		this.setState({ newOrigem: "" });
+	}
+
+	componentDidMount() {
+		this.setState({ lastKey: this.getLastKey() });
 	}
 
 	render() {
@@ -33,55 +69,65 @@ export default class CadastroItemScreen extends Component {
 					resizeMode="cover"
 					style={styles.image}
 				>
-					<View style={styles.rect}>
-						<Content style={{ margin: 25 }}>
-							<Text style={{ fontWeight: '700', fontSize: 20, marginBottom: '10%' }}> 
-								Entre com os dados para adicionar uma nova entrega de mercadoria: 
+					<ScrollView>
+						<View style={styles.rect}>
+							<Content style={{ margin: 25 }}>
+								<Text style={styles.headerText}>
+									Entre com os dados para adicionar uma nova entrega de mercadoria:
 							</Text>
-							<Form>
-								<Item rounded style={styles.inputBox}>
-									<Input
-										placeholder='Nome do Cliente'
-										value={this.state.newNome}
-										onChangeText={(newNome)=> this.setState({ newNome: newNome })}
-									/>
-								</Item>
-								<Item rounded style={styles.inputBox}>
-									<TextInputMask
-										refInput={ref => { this.input = ref }}
-										value={this.state.newData}
-										onChangeText={(formatted, extracted) => {
-											this.setState({ newData: formatted })
-										}}
-										mask={"[31]/[12]/[2099]"}
-										placeholder={'Data de Entrega'}
-										style={{ fontSize: 15, marginLeft: 5 }}
-									/>
-								</Item>
-								<Item rounded style={styles.inputBox}>
-									<Input
-										placeholder='Endereço de Partida'
-										value={this.state.newPartida}
-										onChangeText={(newPartida) => { this.setState({ newPartida: newPartida }) }}
-									/>
-								</Item>
-								<Item rounded style={styles.inputBox}>
-									<Input
-										placeholder='Endereço de Destino'
-										value={this.state.newDestino}
-										onChangeText={(newDestino) => { this.setState({ newDestino: newDestino }) }}
-									/>
-								</Item>
-							</Form>
-							<View style={{ marginBottom: 100 }}/>
-							<Button success style={{ marginStart: 25, marginEnd: 25, justifyContent: 'space-evenly' }}> 
-								<Icon name={'ios-checkbox'}/>
-								<Text style={{ color: '#FFFFFF', fontSize: 17, fontWeight: "bold", }}>
-									Adicionar
-								</Text>
-							</Button>
-						</Content>
-					</View>
+								<Form>
+									<Item rounded style={styles.inputBox}>
+										<Input
+											placeholder='Nome do Cliente'
+											value={this.state.newNome}
+											onChangeText={(newNome) => this.setState({ newNome: newNome })}
+										/>
+									</Item>
+									<Item rounded style={styles.inputBox}>
+										<TextInputMask
+											refInput={ref => { this.input = ref }}
+											value={this.state.newData}
+											onChangeText={(formatted, extracted) => {
+												this.setState({ newData: formatted })
+											}}
+											mask={"[00]/[00]/[0000]"}
+											placeholder={'Data de Entrega'}
+											style={{ fontSize: 15, marginLeft: 5 }}
+										/>
+									</Item>
+									<Item rounded style={styles.inputBox}>
+										<Input
+											placeholder='Endereço de Partida'
+											value={this.state.newOrigem}
+											onChangeText={(newOrigem) => { this.setState({ newOrigem: newOrigem }) }}
+										/>
+									</Item>
+									<Item rounded style={styles.inputBox}>
+										<Input
+											placeholder='Endereço de Destino'
+											value={this.state.newDestino}
+											onChangeText={(newDestino) => { this.setState({ newDestino: newDestino }) }}
+										/>
+									</Item>
+								</Form>
+								<View style={{ marginBottom: 100 }} />
+								<Button
+									success
+									style={styles.confirmationButton}
+									onPress={() => {
+										this.addNewItemInList();
+										ToastAndroid.show('Adicionado com sucesso! ', ToastAndroid.SHORT);
+										this.clearAllInputBoxes();
+									}}
+								>
+									<Icon name={'ios-checkbox'} />
+									<Text style={{ color: '#FFFFFF', fontSize: 17, fontWeight: "bold", }}>
+										Adicionar
+									</Text>
+								</Button>
+							</Content>
+						</View>
+					</ScrollView>
 				</ImageBackground>
 			</Container>
 		);
@@ -106,5 +152,16 @@ const styles = StyleSheet.create({
 		marginTop: 10,
 		alignItems: 'stretch'
 
+	},
+	headerText: {
+		fontWeight: '700',
+		fontSize: 20,
+		marginBottom: '10%'
+	}
+	,
+	confirmationButton: {
+		marginStart: 25,
+		marginEnd: 25,
+		justifyContent: 'center'
 	}
 });
